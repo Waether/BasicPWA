3169f53e820849a081637a362ca698e980622bfd
Waether <nathan.hautbois@epitech.eu>
2020-03-31 15:33:58 +0200

    stripped down to the essential

---
+++ app/public/scripts/handle_install.js
@@ -0,0 +1,28 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(function() { console.log('Service Worker Registered'); });
+}
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', (e) => {
+        addBtn.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the install prompt');
+            } else {
+                console.log('User dismissed the install prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
--- app/sw.js
+++ app/sw.js
@@ -3,12 +3,15 @@ self.addEventListener('install', function(e) {
         caches.open('video-store').then(function(cache) {
             return cache.addAll([
                 '/basicpwa/',
-                '/basicpwa/server.js',
-                '/basicpwa/views/index.html',
-                '/basicpwa/public/style.css',
-                '/basicpwa/public/script.js',
+                '/basicpwa/index.html',
+                '/basicpwa/sw.js',
+                '/basicpwa/public/icons/',
                 '/basicpwa/public/icons/icon-32.png',
-                '/basicpwa/public/icons/icon-512.png'
+                '/basicpwa/public/icons/icon-512.png',
+                '/basicpwa/public/scripts/',
+                '/basicpwa/public/scripts/index.js',
+                '/basicpwa/public/stylesheet/',
+                '/basicpwa/public/stylesheet/style.css'
             ]);
         })
     );
