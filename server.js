6630cf8f9bf0098d1d20de40d6305921d3407bfc
Waether <nathan.hautbois@epitech.eu>
2020-03-31 16:13:16 +0200

    fixed server

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ const https = require('https');
 const fs = require('fs');
 const app = express();
 
-// app.use('/', express.static('app/'));
-//
-// app.get('/', (request, response) => {
-//     response.sendFile(__dirname + "/app/index.html");
-// });
-//
-// const listener = app.listen(process.env.PORT, () => {
-//     console.log("Your app is listening on port " + listener.address().port);
-// });
+app.use('/', express.static('app/'));
 
-app.get('/', function (req, res) {
-    res.send('hello world')
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + "/app/index.html");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+    console.log("Your app is listening on port " + listener.address().port);
 });
 
 https.createServer({
-    key: fs.readFileSync('/etc/ssl/private/pwa-selfsigned.key'),
-    cert: fs.readFileSync('/etc/ssl/certs/pwa-selfsigned.crt')
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
 }, app)
     .on('error', function(err) {
         console.error(err)
