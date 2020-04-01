a3d033efabd99153acf462a1c91db05570d1fd6b
Waether <nathan.hautbois@epitech.eu>
2020-04-01 16:15:52 +0200

    Basic server

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const app = express();
@@ -10,24 +9,8 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + "/app/index.html");
 });
 
-
-const privateKey = fs.readFileSync('privkey.pem', 'utf8');
-const certificate = fs.readFileSync('cert.pem', 'utf8');
-const ca = fs.readFileSync('chain.pem', 'utf8');
-
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
-
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80');
-});
-
-httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
 });
