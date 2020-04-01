30c3c7feef79785fd553a5de5415a106d8c0b9e0
Waether <nathan.hautbois@epitech.eu>
2020-04-01 12:04:43 +0200

    Updated service worker

--- app/sw.js
+++ app/sw.js
@@ -1,23 +1,34 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('video-store').then(function(cache) {
-            return cache.addAll([
-                '/index.html',
-                '/sw.js',
-                '/public/icons/icon-32.png',
-                '/public/icons/icon-512.png',
-                '/public/scripts/index.js',
-                '/public/stylesheet/style.css'
-            ]);
-        })
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/sw.js',
+    '/public/icons/icon-32.png',
+    '/public/icons/icon-512.png',
+    '/public/scripts/index.js',
+    '/public/stylesheet/style.css'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', function(e) {
-    console.log(e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
     );
 });
