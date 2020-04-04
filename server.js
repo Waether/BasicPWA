14965b061887a062884e8254cd29f6e5cb3d1f46
Waether <nathan.hautbois@epitech.eu>
2020-04-04 13:53:57 +0200

    Stored user request

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const util = require('util');
 const app = express();
 const bodyParser = require('body-parser');
 
+let UserNotifications = [];
+
 app.use('/', express.static('app/'));
 
 app.use(bodyParser.urlencoded({
@@ -17,8 +19,23 @@ app.get('/', (request, response) => {
 });
 
 app.post('/public/addFeed', (request, response) => {
-    console.log("Body : " + util.inspect(request.body, false, null, true /* enable colors */));
-    response.send("Received")
+    if (request.body.subscription && request.body.feed) {
+        for (const elem of UserNotifications) {
+            if (elem.subscription.endpoint === request.body.subscription.endpoint) {
+                response.send("Received");
+                return;
+            }
+        }
+        UserNotifications.push({subscription: request.body.subscription, link: request.body.feed});
+    }
+
+    response.send("Received");
+
+    console.log("Listing Users ...");
+    UserNotifications.forEach(elem => {
+        console.log("Subscription : " + util.inspect(elem.subscription, false, null, true /* enable colors */));
+        console.log("Link : " + elem.link);
+    });
 });
 
 const httpServer = http.createServer(app);
@@ -26,24 +43,23 @@ const httpServer = http.createServer(app);
 httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80');
 });
-
-let Parser = require('rss-parser');
-let parser = new Parser();
-
-var nIntervId;
-
-nIntervId = setInterval(getrss, 6000);
-
-
-async function getrss() {
-
-    let feed = await parser.parseURL('https://www.reddit.com/r/all.rss');
-
-    let now = Date();
-    let b = now.toISOString();
-
-    console.log(b);
-
-    console.log(feed.items[feed.items.length -1].pubDate);
-    console.log(feed.items[feed.items.length -1].isoDate);
-}
+//
+// let Parser = require('rss-parser');
+// let parser = new Parser();
+//
+// var nIntervId;
+//
+// nIntervId = setInterval(getrss, 6000);
+//
+// async function getrss() {
+//
+//     let feed = await parser.parseURL('https://www.reddit.com/r/all.rss');
+//
+//     let now = Date();
+//     let b = now.toISOString();
+//
+//     console.log(b);
+//
+//     console.log(feed.items[feed.items.length -1].pubDate);
+//     console.log(feed.items[feed.items.length -1].isoDate);
+// }
